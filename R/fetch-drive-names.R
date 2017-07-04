9d1ab2aaf07924f6efb4da88cd85b6e62ea89cdc
rosseji <rosseireland@gmail.com>
2017-07-04 12:24:57 +1000

    first draft...

---
+++ R/fetch-drive-names.R
@@ -0,0 +1,23 @@
+
+#' Fetch the name of the second HD as a string
+#'
+#' @name fetch_second_drive
+#' @return Name as a string in the form "x:"
+#' @export
+
+
+fetch_second_drive <- function() {
+
+  system("wmic LOGICALDISK LIST BRIEF", intern = T) %>%
+    as.list() %>%
+    map(~str_split(., " ") %>%
+          unlist()) %>%
+    lapply(function(x) {x[x != ""]}) %>%
+    tibble(DeviceID = map(., 1),
+           Used = map(., 3)) %>%
+    select(DeviceID, Used) %>%
+    filter(str_count(DeviceID) == 2) %>%
+    filter(str_count(Used) > 1) %>%
+    pull(DeviceID) %>%
+    `[[`(2)
+}
