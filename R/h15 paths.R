0b00209392f8ba265670b63c449ae2cc737defe5
rosseji <rosseireland@gmail.com>
2017-07-31 11:52:13 +1000

    add

---
+++ R/h15 paths.R
@@ -0,0 +1,15 @@
+
+#' Saves all required file paths to parent envrioment
+#'
+#' Uses the ysi \code{paths} library and functions to write file paths strings to parent envrioment.
+#'
+#' @name h15_paths
+#' @return Multiple paths as string objects
+#' @export
+
+h15_paths <- function(){
+  paths::DropboxDirFN("Dropbox (YSI)/YSI Team Folder/Content/R/HILDA Index/")
+  paths::fetch_second_drive()
+  index.path <<- glue("{DropboxDir}xwave index.csv")
+  archive.path <<- glue("{DropboxDir}archive/xwave index.csv")
+}
